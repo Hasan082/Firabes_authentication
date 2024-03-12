48b9c461533d396253d9be044e17408ee47a56bb
Md Hasanuzzaman <dr.has82@gmail.com>
2024-03-12 12:50:17 +0600

    Firebase signup done

---
+++ app/src/main/java/com/hasan/exam/SignUpActivity.java
@@ -0,0 +1,46 @@
+package com.hasan.exam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+    EditText fullName, username, password;
+    Button signup;
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        fullName = findViewById(R.id.fullName);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        signup = findViewById(R.id.signup);
+        mAuth = FirebaseAuth.getInstance();
+
+        signup.setOnClickListener(v -> mAuth.createUserWithEmailAndPassword(username.getText().toString().trim(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(SignUpActivity.this, "Signup Success!", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
+                }else {
+                    Toast.makeText(SignUpActivity.this, "Signup Failed! Try Again!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }));
+
+    }
+}
